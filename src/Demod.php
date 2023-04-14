75a178faad2152883bdb203a49eff0faa161b19a
shangguanmuan <shangguanmuan@modo7game.com>
2023-04-14 15:28:40 +0800

    demo

---
+++ src/Demod.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace sgma1\demo1package;
+
+class Demod
+{
+
+
+    public static $testStr = 'hello world testStr';
+
+    public function __construct()
+    {
+        echo 'hello world';
+    }
+
+
+    public function testFunc()
+    {
+        return 'hello world testFunc';
+    }
+}
